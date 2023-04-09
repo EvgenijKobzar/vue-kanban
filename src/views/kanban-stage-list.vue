cbb95b7f8a4129cba39c790f85e07e5e3b00d640
EvgenijKobzar <9097963939@rambler.ru>
2023-04-09 21:55:23 +0200

    Update kanban-stage-list.vue

--- src/views/kanban-stage-list.vue
+++ src/views/kanban-stage-list.vue
@@ -9,35 +9,41 @@
 				v-if="!state.chip1 && !state.chip2 && !state.chip3"
 				color="primary"
 				dark
-				@click="state.chip1 = true, state.chip2 = true, state.chip3 = true, state.snackbar = false"
+				@click="addChips()"
 		>
+<!--			@click="state.chip1 = true, state.chip2 = true, state.chip3 = true, state.snackbar = false"-->
 			Reset Chips
 		</v-btn>
 		<v-snackbar
 				v-model="state.snackbar"
 				:content-class="'ma-4 mx-2'"
 				:timeout="1000000"
+
 		>
-			<v-chip v-model="state.chip1" class="mx-2" size="small" closable >
-				Р406КВ 39
-			</v-chip>
-			<v-chip v-model="state.chip2" class="mx-2" size="small" closable >
-				М864КТ 39
-			</v-chip>
-			<v-chip v-model="state.chip3" class="mx-2" size="small" closable >
-				6 апр
+			<v-chip class="mx-2" size="small" v-for="chip in state.state.chips" :key="chip.id">
+				{{chip.title}}
 			</v-chip>
 
+			<template v-slot:actions>
+				<v-btn
+						size="small"
+
+					density="compact"
+						icon="mdi-close"
+						
+				/>
+			</template>
 		</v-snackbar>
 	</div>
 
-	<KanbanStageItem v-for="item in items" :item="item"/>
+	<KanbanStageItem v-for="item in items" :item="item" />
 </template>
 
 <script setup>
 	import KanbanStageItem from "./kanban-stage-item.vue";
 	import {computed, reactive} from 'vue'
 	import { useStore } from 'vuex'
+	import ColorTheme from "../lib/color-theme.js";
 
 	const store = useStore()
 	const items = computed(() => store.getters.getStages)
@@ -46,10 +52,23 @@
 		state: {
 			snackbar: false,
 			text: `Hello, I'm a snackbar`,
-			chip1: true,
-			chip2: true,
-			chip3: true,
+			chips:[
+					{title:'Р406КВ 39', id: 1,},
+					{title:'М864КТ 39', id: 2,},
+					{title:'6 апр', id: 3,},
+			]
 		}
+	})
+
+	function addChips()
+	{
+		state.state.chips.push({title:'Р406КВ 39', id: 4,})
+	}
+
+
+	const chips = computed(() => {
+
+		return state.chips
 	})
 
 </script>

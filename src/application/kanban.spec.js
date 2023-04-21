f03f72d455b2aa64c1330d3629b2238270813e5f
EvgenijKobzar <9097963939@rambler.ru>
2023-04-21 17:58:07 +0200

    spec Action Init - Called Once

--- src/application/kanban.spec.js
+++ src/application/kanban.spec.js
@@ -1,19 +1,46 @@
-import { describe, it, expect} from "vitest";
+import { afterEach, describe, it, expect, vi} from "vitest";
 import { mount } from "@vue/test-utils"
-// import component from "./kanban.vue";
-import component from "../views/elements/plus.vue";
-
+import component from "./kanban.vue";
 
 import { createVuetify } from 'vuetify'
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
 
+import { createStore } from 'vuex'
+import Stage from "../store/modules/stage";
+
+const Module = new Stage();
+const actions = Module.getActions();
+
+vi.spyOn(actions, 'init')
+
+const store = createStore({
+	modules: {
+		[Module.getName()]: {
+			state: Module.getState(),
+			getters: Module.getGetters(),
+			actions,
+			mutations: Module.getMutations(),
+		}
+	},
+})
+
+
 describe('Stage list ', () => {
+
+	afterEach(() => {
+		vi.resetAllMocks()
+	})
+
 	const vuetify = createVuetify({ components, directives })
 	it('mounts without errors', () => {
-		const view = mount(component, {global: {plugins: [vuetify]},})
-		// console.log(view.html())
-		expect(view).toBeDefined()
+		const view = mount(component, {global: {plugins: [vuetify, store]},})
+		expect(view).toBeDefined();
+		expect(actions.init).toHaveBeenCalledOnce();
+	});
+	it('Action Init - Called Once', () => {
+		mount(component, {global: {plugins: [vuetify, store]},})
+		expect(actions.init).toHaveBeenCalledOnce();
 	})
 })
